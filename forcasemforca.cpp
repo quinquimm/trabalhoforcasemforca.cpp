2c0266312d2732ae251dd22d3814b265603d59ba
quinquimm <quinquim07@gmail.com>
2025-02-05 00:05:06 -0100

    Update forcasemforca.cpp

--- forcasemforca.cpp
+++ forcasemforca.cpp
@@ -5,42 +5,32 @@ using namespace std;
 string frase;
 char letra;
 
-struct letrasfr {
-    char l;
-    bool ctrl;
-};
-
 int main() {
-    cout << "Bem vindo ao jogo da forca só que sem forca." << endl;
+    cout << "Bem-vindo ao jogo da forca só que sem forca." << endl;
     cout << "Digite uma frase para o próximo jogador adivinhar." << endl;
     getline(cin, frase);
 
-    system("clear");
+    system("cls");
 
-    string frase_mostrada = frase;
+    string frase_oculta = frase;
 
-    for (char &l : frase_mostrada) { 
+    for (char &l : frase_oculta) {
         if (l != ' ') {
             l = '_';
         }
     }
-    
-    for(char c : frase_mostrada){
-        cout << c << " ";
-    }
 
-    cout << "\n\n";
+    cout << frase_oculta << "\n\n";
 
     while (true) {
         cout << "Tente adivinhar a frase!" << endl;
-        cout << "Boa sorte!" << endl;
         cin >> letra;
 
         bool acertou = false;
 
         for (size_t i = 0; i < frase.size(); i++) {
             if (frase[i] == letra) {
-                frase_mostrada[i] = letra;
+                frase_oculta[i] = letra;
                 acertou = true;
             }
         }
@@ -49,13 +39,15 @@ int main() {
             cout << "A letra '" << letra << "' não está na frase. Tente novamente!\n";
         }
 
-        cout << frase_mostrada << "\n\n";
+        system("cls");
 
-        if (frase_mostrada == frase) {
+        cout << frase_oculta << "\n\n";
+
+        if (frase_oculta == frase) {
+            system("cls");
             cout << "\nParabéns! Você adivinhou a frase: " << frase << endl;
             break;
         }
     }
-
     return 0;
 }
